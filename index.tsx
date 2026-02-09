9d298ada96da3d10725c68305a8e8a1a93ec088a
audiovoicemed-byte <audiovoice.med@gmail.com>
2026-02-09 18:51:28 -0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,18 +1,16 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// O React procura o ID 'root' que você colocou no index.html
 const rootElement = document.getElementById('root');
-
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  // Isso ajuda a diagnosticar se o HTML está correto
-  console.error("Erro: O elemento 'root' não foi encontrado no index.html");
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
 }
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
